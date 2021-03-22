86e7a8a411afeb12ba20c6a20185fd954474abcc
荀徒之 <kalxd@126.com>
2021-03-22 21:47:22 +0800

    一个简单的例子。

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,3 +1,14 @@
+use gtk::prelude::*;
+use gtk::{Application, Window};
+
+use golduck::gtk_app_run;
+
+fn app_run(app: &Application) {
+	let window = Window::new(gtk::WindowType::Toplevel);
+	window.set_application(Some(app));
+	window.show_all();
+}
+
 fn main() {
-	println!("hello world");
+	gtk_app_run("example.gtk.demo", app_run);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
-#[cfg(test)]
-mod tests {
-	#[test]
-	fn it_works() {
-		assert_eq!(2 + 2, 4);
-	}
+use gio::prelude::{ApplicationExt, ApplicationExtManual};
+use gtk::Application;
+
+pub fn gtk_app_run<F>(id: &str, f: F)
+where
+	F: Fn(&Application) + 'static,
+{
+	let app = Application::new(Some(id), gio::ApplicationFlags::FLAGS_NONE)
+		.expect("gtk init failed within goduck lib!");
+
+	app.connect_activate(f);
+
+	app.run(&[]);
 }
