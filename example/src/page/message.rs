8b3b6ac11b01f667dbd488ecc757ba9f04dd0197
荀徒之 <kalxd@126.com>
2021-04-10 21:37:37 +0800

    AlertI示例。

--- example/src/page/message.rs
+++ example/src/page/message.rs
@@ -1,10 +1,11 @@
 use gtk::prelude::*;
 use gtk::Window;
 
-use golduck::{builder::BuilderI, include_builder};
+use golduck::{alert::AlertI, builder::BuilderI, include_builder};
 
 use super::IsPage;
 
+#[derive(Clone)]
 pub struct MessagePage {
 	window: Window,
 }
@@ -13,7 +14,42 @@ pub fn new() -> MessagePage {
 	let builder = include_builder!("../../ui/message.glade");
 	let window = builder.get_by::<Window>("window");
 
-	MessagePage { window }
+	let page = MessagePage { window };
+
+	{
+		let page = page.clone();
+		builder
+			.get_button_by("normal_btn")
+			.connect_clicked(move |_| {
+				page.show_alert("这是一个提示。");
+			});
+	}
+
+	{
+		let page = page.clone();
+		builder
+			.get_button_by("error_btn")
+			.connect_clicked(move |_| {
+				page.show_error("你看到了一个错误提示！");
+			});
+	}
+
+	{
+		let page = page.clone();
+		builder.get_button_by("warn_btn").connect_clicked(move |_| {
+			page.show_warn("你看了一个警告。");
+		});
+	}
+
+	return page;
+}
+
+impl AlertI for MessagePage {
+	type Parent = Window;
+
+	fn get_parent(&self) -> Option<&Self::Parent> {
+		Some(&self.window)
+	}
 }
 
 impl IsPage for MessagePage {
